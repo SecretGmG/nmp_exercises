17c1110a25a9b3dab10e0a8e328afca267fa65a4
SecretGmG <cedric@sigrist.ch>
2025-03-21 12:27:40 +0100

    started ex2, renamed folders

---
+++ ex_02/nmp_util.py
@@ -0,0 +1,114 @@
+import pandas as pd
+from functools import wraps
+import matplotlib.pyplot as plt
+from matplotlib.axes import Axes
+import numpy as np
+import sympy
+from typing import Iterable, Tuple, List
+
+import seaborn as sns
+sns.set_theme()
+
+# File i will keep continuously updating during the course, adding usefull functions
+# the docstrings were written with deepseek
+
+
+## SERIE 1
+def get_inliers(data : np.ndarray, max_std : float= 4.0, iterative : bool = True):
+    """
+    Identifies inliers in a dataset based on the standard deviation.
+
+    Args:
+        data (np.ndarray): Input data array.
+        max_std (float): Maximum number of standard deviations allowed for inliers. Default is 4.0.
+        iterative (bool): If True, iteratively refines inliers. Default is True.
+
+    Returns:
+        np.ndarray: Boolean array indicating inliers (True) and outliers (False).
+    """
+    # start with the entire data as inliers
+    inliers = np.ones_like(data, dtype=bool)
+    
+    while True:
+        std = data[inliers].std()
+        mean = data[inliers].mean()
+        
+        new_inliers = np.abs(data-mean) < max_std*std
+        
+        # stop iterating if specified or if no inliers removed
+        if (not iterative) or np.all(new_inliers == inliers):
+            inliers = new_inliers
+            break
+        
+        inliers = new_inliers
+        
+    return inliers
+
+
+
+## SERIE 2
+
+def error_propagation_formula(f : sympy.Matrix|Iterable[sympy.Expr], args : List[sympy.Symbol]) -> Tuple[sympy.Expr, sympy.MatrixSymbol]:
+    """
+    Computes the error propagation formula A * K * A.T for a given expression matrix (vector).
+
+    Args:
+        f: Vector-valued expression matrix or an iterable of expressions.
+        args: List of symbols for the Jacobian.
+
+    Returns:
+        Tuple containing:
+            - The formula A * K * A.T.
+            - The symbolic covariance matrix K.
+    """
+    
+    if not isinstance(f, sympy.Matrix):
+        #assume that f is an iterable of expressions if it is not already a matrix
+        #in this case it should be possible to simly convert into a collimn matrix
+        f = sympy.Matrix(list(f))
+    
+    A = f.jacobian(args)
+    K = sympy.MatrixSymbol('K',len(args), len(args))
+    return A * K * A.T , K #skript S.12
+
+
+@wraps(pd.read_csv)
+def read_dat(*args, **kwargs):
+    return pd.read_csv(*args, index_col=False, sep='\s+', **kwargs)
+
+#remove the arrowheads and set the pivot to mid to vizualize eigenvectors more effectively
+EIGEN_VEC_QUIVER_KWARGS = {'headwidth' : 0, 'headlength' : 0, 'headaxislength' : 0, 'pivot' : 'mid'}
+
+
+def matrix_quiver(x : np.ndarray, y: np.ndarray, matrices : np.ndarray, shade_determinant = False):
+    """
+    Visualizes eigenvectors of matrices using quiver plots.
+
+    Args:
+        x (np.ndarray): X-coordinates for the quiver plot.
+        y (np.ndarray): Y-coordinates for the quiver plot.
+        matrices (np.ndarray): Matrices for which eigenvectors are computed.
+        ax (Axes): Matplotlib axes object. If None, the current axes are used.
+        shade_determinant (bool): If True, shades the plot based on the determinant of the matrices.
+
+    Returns:
+        None
+    """
+    
+    eigenvalues, eigenvectors = np.linalg.eig(matrices)
+    # from the docs of np.linalg.eig : eigenvectors[:,i] is the eigenvector corresponding to the eigenvalue eigenvalues[i] !!!
+
+    #scale by the eigenvalues
+    scaled_eigenvectors = eigenvectors * eigenvalues[...,None,:]
+
+    if shade_determinant:
+        mesh = plt.pcolormesh(x, y, eigenvalues[...,0]*eigenvalues[...,1], alpha=0.5)
+        plt.colorbar(mesh, label = 'determinant [mm$^2$]')
+
+    plt.quiver(x, y, scaled_eigenvectors[...,0,0], scaled_eigenvectors[...,1,0], **EIGEN_VEC_QUIVER_KWARGS).set_label('eigenvalues')
+    plt.quiver(x, y, scaled_eigenvectors[...,0,1], scaled_eigenvectors[...,1,1], **EIGEN_VEC_QUIVER_KWARGS)
+
+# a little trick to make importing the standard stuff a bit easier using from nmp_util import *
+__all__ = [
+    'np', 'pd', 'sympy','plt' ,'sns'
+]
