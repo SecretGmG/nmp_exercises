c3a0eed7d2669cd925781fcb7c8c780b40bd2625
SecretGmG <cedric@sigrist.ch>
2025-04-03 13:40:28 +0200

    solved exercise 3

--- ex_03/nmp_util.py
+++ ex_03/nmp_util.py
@@ -2,11 +2,14 @@ from dataclasses import dataclass
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import scipy.linalg
+import scipy.sparse
 import seaborn as sns
 sns.set_style()
 import sympy
+import scipy
 from functools import wraps
-from typing import Iterable, Tuple, List
+from typing import Iterable, Literal, Tuple, List
 #improve readability of the output of numpy arrays in jupyter notebooks
 np.set_printoptions(linewidth=150)
 
@@ -140,43 +143,42 @@ def matrix_quiver(x : np.ndarray, y: np.ndarray, matrices : np.ndarray, shade_de
         
 ## Serie 3
 
-
 @dataclass
 class LinearModelResult:
     """
     Class to hold parameters for a linear model.
     """
     parameters : np.ndarray
-    m_0_sr : float
+    rse : float # Residual Standard Error = m_0
     normal_matrix : np.ndarray
-    residuals : np.ndarray
+    residuals : np.ndarray | None = None
     
-    
-    def covariance_matrix(self) -> np.ndarray:
-        """
-        Computes the covariance matrix of the parameters.
-
-        Returns:
-            np.ndarray: Covariance matrix.
-        """
-        return self.m_0_sr * np.linalg.inv(self.normal_matrix)
-
     @property
-    def m_0(self) -> float:
-        return np.sqrt(self.m_0_sr)
-    
-    def __repr__(self):
-        return f"LinearModelResult(parameters = \n{self.parameters}\nm_0 = {self.m_0})\n normal_matrix =\n {self.normal_matrix}"
-def poly_design_matrix(degree, features) -> np.ndarray:
-    return np.column_stack([features**i for i in reversed(range(degree+1))])
+    def cofactor_matrix(self) -> np.ndarray:
+        return scipy.linalg.inv(self.normal_matrix)
 
-def design_matrix(f : sympy.Expr, features : np.ndarray, parameters : List[sympy.Symbol], feature_sym = sympy.Symbol) -> np.ndarray:
+    def __repr__(self):
+        return f"LinearModelResult(parameters = \n{self.parameters}\nm_0 = {self.rse})\n normal_matrix =\n {self.normal_matrix}"
+    
+def poly_design_matrix(x : np.ndarray, degree : int) -> np.ndarray:
+    """Computes the design matrix for a polynomial of a given degree.
+
+    Args:
+        x (np.ndarray): input values.
+        degree (int): degree of the polynomial.
+
+    Returns:
+        np.ndarray: Design matrix.
     """
-    Computes the design matrix for a given function and input features.
+    return np.column_stack([x**i for i in reversed(range(degree+1))])
+
+def design_matrix(f : sympy.Expr, x : np.ndarray, parameters : List[sympy.Symbol], feature_sym = sympy.Symbol) -> np.ndarray:
+    """
+    Computes the design matrix for a given function and input x.
     
     Args:
         f (sympy.Expr): Expression for the function.
-        features (np.ndarray): Input features.
+        x (np.ndarray): Input x.
         parameters (List[sympy.Symbol]): List of symbols representing the parameters.
         feature_sym (sympy.Symbol): Symbol representing the feature variable.
     Return:
@@ -184,58 +186,92 @@ def design_matrix(f : sympy.Expr, features : np.ndarray, parameters : List[sympy
     """
     column_functions = [sympy.lambdify([feature_sym], sympy.diff(f, a)) for a in parameters]
     
-    design_matrix_columns = [np.broadcast_to(column_function(features), features.shape) for column_function in column_functions]
+    design_matrix_columns = [np.broadcast_to(column_function(x), x.shape) for column_function in column_functions]
     
     design_matrix = np.column_stack(design_matrix_columns)
     return design_matrix
 
-def normal_equation(y : np.ndarray, A : np.ndarray) -> LinearModelResult:
+def normal_equation(A : np.ndarray, y : np.ndarray) -> LinearModelResult:
     """
     Computes the parameters of a linear model using the normal equation method.
     
     Args:
-        y (np.ndarray): Target variable.
         A (np.ndarray): Design matrix.
+        y (np.ndarray): Target variable.
+        weights (np.ndarray | float | None) The covariance of the data, if None uses identity default is None
     
     Returns:
         LinearModelResult: Parameters of the linear model.
     """
     b = A.T@y
     N = A.T@A
+    
     parameters = np.linalg.solve(N, b)
     residuals =  A@parameters - y
     m_0_sr = (residuals.T@residuals) / (len(y) - len(parameters))
-    return LinearModelResult(parameters, m_0_sr, N, residuals)
+    
+    return LinearModelResult(
+        parameters,
+        m_0_sr**0.5,
+        N,
+        residuals)
 
-def poly_fit(degree, y, features) -> LinearModelResult:
+def _get_N_b(x : np.ndarray, y : np.ndarray, degree : int) -> Tuple[np.ndarray, np.ndarray]:
+    """ Computes the normal matrix and the vector b directly for polynomial fitting.
+
+    Args:
+        x (np.ndarray): input features.
+        y (np.ndarray): target variable.
+        degree (int): degree of the polynomial.
+
+    Returns:
+        Tuple containing:
+            - np.ndarray: Normal matrix.
+            - np.ndarray: Vector b.
+    """
+    b = np.ndarray(degree+1)
+    for i in range(degree+1):
+        b[degree-i] = np.sum(y * x**i)
+    
+    #precompute sums of powers since the Matrix N reuses them
+    N_entries = np.ndarray(degree*2+1)
+    for i in range(degree*2+1):
+        N_entries[i] = np.sum(x**i)
+    
+    N = np.ndarray((degree+1, degree+1))
+    
+    for i in range(degree+1):
+        for j in range(degree+1):
+            N[degree-i,degree-j] = np.sum(N_entries[i+j])
+    return N, b
+
+def poly_fit(x : np.ndarray, y : np.ndarray, degree : int, method : Literal['design matrix','direct'] = 'direct') -> LinearModelResult:
     """
     Fits a polynomial of a given degree to the data using the normal equation method.
     Does not generate a design matrix, saving memory.
 
     Args:
-        degree (int): degree of the polynomial to fit.
+        x (np.ndarray): input features.
         y (np.ndarray): target variable.
-        features (np.ndarray): input features.
+        degree (int): degree of the polynomial to fit.
+        method Lietarl: method to use for fitting.
+            - 'design matrix': uses the design matrix method.
+            - 'direct': uses the direct method.
 
     Returns:
         LinearModelResult: Parameters of the polynomial fit.
     """
+    if method == 'design matrix':
+        #use the design matrix method
+        A : np.ndarray = poly_design_matrix(x, degree)
+        return normal_equation(A, y)
+    if method == 'direct':
+        N, b = _get_N_b(x, y, degree)
     
-    b = np.array([
-            np.sum(x*y**i for x,y in zip(y, features))
-        for i in reversed(range(degree+1))]
-        )
+        parameters = np.linalg.solve(N, b)
+        residuals =  np.polyval(parameters, x) - y
+        m_0_sr = (residuals.T @ residuals) / (len(y) - len(parameters))
     
-    #precompute sums of powers since the Matrix N reuses them
-    N_entries = [np.sum(x**i for x in features) for i in range(degree*2+1)]
+        return LinearModelResult(parameters, m_0_sr**0.5, N, residuals)
     
-    N = np.array([[
-            N_entries[i+j]
-        for i in reversed(range(degree+1))] 
-        for j in reversed(range(degree+1))]
-        )
-    
-    parameters = np.linalg.solve(N, b)
-    residuals =  np.polyval(parameters, features) - y
-    m_0_sr = (residuals.T @ residuals) / (len(y) - len(parameters))
-    return LinearModelResult(parameters, m_0_sr, N, residuals)
+    raise Exception(f'method {method} not supported')
